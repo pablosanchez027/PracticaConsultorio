a966757e9848883401c4bccac574669deef68bbb
Pablo Sanchez <pablosanchez027@hotmail.com>
2018-11-06 08:38:04 -0700

    Agregar archivos de proyecto.

---
+++ PracticaConsultorio/ElementoConsultaControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaConsultorio
+{
+    /// <summary>
+    /// Lógica de interacción para ElementoConsultaControl.xaml
+    /// </summary>
+    public partial class ElementoConsultaControl : UserControl
+    {
+        Consulta consulta;
+        public ElementoConsultaControl(Consulta consulta)
+        {
+            InitializeComponent();
+            this.consulta = consulta;
+
+            lblSintomas.Text = consulta.Sintomas;
+            lblDiagnostico.Text = consulta.Diagnostico;
+            lblReceta.Text = consulta.Receta;
+            lblFecha.Text = consulta.Fecha.ToString();
+        }
+    }
+}
---
+++ PracticaConsultorio/HistorialConsultasWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PracticaConsultorio
+{
+    /// <summary>
+    /// Lógica de interacción para HistorialConsultasWindow.xaml
+    /// </summary>
+    public partial class HistorialConsultasWindow : Window
+    {
+        Paciente paciente;
+        public HistorialConsultasWindow(Paciente paciente)
+        {
+            InitializeComponent();
+            this.paciente = paciente;
+
+            lblNombrePaciente.Text = paciente.Nombre;
+
+            foreach(Consulta consulta in Datos.consultas)
+            {
+                var nuevoElementoConsulta = new ElementoConsultaControl(consulta);
+                stckHistorialConsultas.Children.Add(nuevoElementoConsulta);
+            }
+        }
+    }
+}
